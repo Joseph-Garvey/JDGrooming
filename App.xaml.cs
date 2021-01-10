0c7ca36ff42bef6c508621ad3446ed52bc14cc59
Joseph Garvey <67016104+Joseph-Garvey@users.noreply.github.com>
2021-01-10 15:48:24 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace JDGrooming
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Window Management
+        /// <summary>
+        /// Listens for key presses on window.
+        /// F11 = Toggle Fullscreen
+        /// </summary>
+        /// <param name="sender">Window</param>
+        /// <param name="e">Key Pressed</param>
+        public void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.F11) { ChangeWindowState((Window)sender); }
+            }
+            catch (InvalidCastException) { }
+            catch (Exception) { }
+        }
+        /// <summary>
+        /// Toggles
+        /// </summary>
+        /// <param name="window"></param>
+        public void ChangeWindowState(Window window)
+        {
+            try
+            {
+                if (RetrieveWindowState(window))
+                {
+                    window.ResizeMode = ResizeMode.CanResize;
+                    window.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    window.ResizeMode = ResizeMode.NoResize;
+                    window.WindowState = WindowState.Maximized;
+                }
+            }
+            catch (NullReferenceException) { }
+            catch (InvalidCastException) { }
+            catch (Exception) { }
+        }
+        /// <summary>
+        /// Minimises the currently active window.
+        /// </summary>
+        public void MinimiseWindow()
+        {
+            try
+            {
+                RetrieveMainWindow().WindowState = WindowState.Minimized;
+            }
+            catch (NullReferenceException) { }
+            catch (InvalidCastException) { }
+            catch (Exception) { }
+        }
+        /// <summary>
+        /// Returns whether the window is fullscreen.
+        /// </summary>
+        /// <param name="window"></param>
+        /// <returns>True if window fullscreen.</returns>
+        public bool RetrieveWindowState(Window window)
+        {
+            if (window.WindowState == WindowState.Maximized) return true;
+            else return false;
+        }
+        /// <summary>
+        /// Returns app mainwindow.
+        /// </summary>
+        private Window RetrieveMainWindow()
+        {
+            return Application.Current.MainWindow;
+        }
+        #endregion
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JDGrooming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Button Events
+        /// <summary>
+        /// Opens Hamburger Menu
+        /// </summary>
+        private void BtnOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            BtnCloseMenu.Visibility = Visibility.Visible;
+            BtnOpenMenu.Visibility = Visibility.Collapsed;
+        }
+        /// <summary>
+        /// Closes Hamburger Menu
+        /// </summary>
+        private void BtnCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            BtnCloseMenu.Visibility = Visibility.Collapsed;
+            BtnOpenMenu.Visibility = Visibility.Visible;
+        }
+        /// <summary>
+        /// Changes Active Display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // try user control v window
+            // grid v viewbox v stackpanel
+            View.Children.Clear();
+            switch (((Button)sender).Content)
+            {
+                case "Window":
+                    //View.Children.Add(new ExampleUserWindow());
+                    break;
+                case "Settings":
+                    //View.Children.Add(new Viewbox { Child = new Register() });
+                    break;
+            }
+        }
+        #endregion
+    }
+}
