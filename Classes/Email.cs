016ee26e252d8528e8b3322c3eaba8d17a645c88
Joseph Garvey <67016104+Joseph-Garvey@users.noreply.github.com>
2021-02-03 17:07:28 +0000

    Email system partially complete.

---
+++ Classes/Email.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDGrooming.Classes
+{
+    public class Email
+    {
+        #region Variables
+        MailAddress address;
+        /// <summary>
+        /// Address to send email to
+        /// </summary>
+        public MailAddress Address { get => address; set => address = value; }
+        /// <summary>
+        /// Address from which to send emails.
+        /// </summary>
+        public MailAddress BusinessAddress = new MailAddress("jddogcareni@gmail.com");
+        public String BusinessAddressPassword = "rwdvM88$fDJh";
+        SmtpClient mailClient = new SmtpClient
+        {
+            Port = 587,
+            Host = "smtp.gmail.com",
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+            DeliveryMethod = SmtpDeliveryMethod.Network
+        };
+        /// <summary>
+        /// MailClient to handle all mail.
+        /// </summary>
+        public SmtpClient MailClient { get => mailClient; set => mailClient = value; }
+        /// <summary>
+        /// 2FA Authentication Code
+        /// </summary>
+        private int AuthenticationNo { get; set; }
+        #endregion
+        // figure out the better way of multithreading this. look at what miles said on discord.
+        #region Constructors
+        public Email(String _address)
+        {
+            Address = new MailAddress(_address);
+            MailClient.Credentials = new NetworkCredential(BusinessAddress.ToString(), BusinessAddressPassword);
+        }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Called to generate and send 2FA email for an account.
+        /// </summary>
+        /// <returns></returns>
+        public int Authenticate()
+        {
+            Generate2FA();
+            BackgroundWorker thread = new BackgroundWorker() { WorkerReportsProgress = true };
+            thread.DoWork += Send2FA;
+            thread.RunWorkerAsync();
+            return AuthenticationNo;
+        }
+        /// <summary>
+        /// Sends email from Business Address to Desired Address.
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="html"></param>
+        private void SendEmail(String subject, String html)
+        {
+            try
+            {
+                MailMessage message = new MailMessage(BusinessAddress.ToString(), Address.ToString());
+                message.Subject = "2FA Authentication";
+                // refine this stuff later
+                message.IsBodyHtml = true;
+                message.Body = html;
+                MailClient.SendMailAsync(message);
+            }
+            // if email doesn't send
+            catch (SmtpException) { }
+            catch (Exception) { }
+        }
+        /// <summary>
+        /// Generates a random int for 2FA
+        /// </summary>
+        private void Generate2FA()
+        {
+            Random r = new Random();
+            AuthenticationNo = r.Next(999);
+        }
+        #endregion
+        #region Events
+        /// <summary>
+        /// Generates 2FA code and sends email.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Send2FA(object sender, DoWorkEventArgs e)
+        {
+            String subject = "Authentication Email from JDDogCare";
+            String html = "Your 2FA authentication code is " + AuthenticationNo  + ".";
+            SendEmail(subject, html);
+        }
+        #endregion
+    }
+}
