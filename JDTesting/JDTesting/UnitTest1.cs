e47ed3395c9bf9555d89018923a51508dbe62e19
Joseph Garvey <67016104+Joseph-Garvey@users.noreply.github.com>
2021-03-24 09:50:31 +0000

    Started shift generation

--- JDTesting/JDTesting/UnitTest1.cs
+++ JDTesting/JDTesting/UnitTest1.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using JDGrooming.Classes.Database_Management;
 
 namespace JDTesting
 {
     [TestClass]
     public class UnitTest1
     {
+        public ProjectDBAccess query = new ProjectDBAccess();
+        public Database db
+        {
+            get { return query.db; }
+            set { query.db = value; }
+        }
         [TestMethod]
         public void TestMethod()
         {
+        }
+        [TestMethod]
+        public void InsertShifts()
+        {
+            bool worked = false;
+            if (db.Connect())
+            {
+                // write code to generate shifts
+            }
+            Assert.IsTrue(worked);
         }
     }
 }
